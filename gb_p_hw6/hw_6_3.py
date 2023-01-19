7273dfa8ba4b207357e1fa6de4f80eaf34586443
And-Rom <you@example.com>
2023-01-19 09:01:35 +0300

    Modded homework task 6-3

--- gb_p_hw6/hw_6_3.py
+++ gb_p_hw6/hw_6_3.py
@@ -5,11 +5,15 @@
 
 #"Иван","Мария","Петр","Илья","Марина","Петр","Алина","Бибочка"
 
+def create_dict(input_data):
+    input_data = sorted((input_data).replace('"','').split(','))
+    keys_list = sorted(set([input_data[i][:1] for i in range(len(input_data))]))
+    output_dict = {key: [] for key in keys_list}
+    for i in range(len(input_data)):
+        for key in output_dict.keys():
+            if key == input_data[i][:1]: output_dict[key].append(input_data[i])
+    return output_dict
 
-input_data = sorted((input('Enter names of employees: ')).replace('"','').split(','))
-keys_list = sorted(set([input_data[i][:1] for i in range(len(input_data))]))
-output_dict = {key: [] for key in keys_list}
-for i in range(len(input_data)):
-    for key in output_dict.keys():
-        if key == input_data[i][:1]: output_dict[key].append(input_data[i])
-print(output_dict)
+value = input('Enter names of employees: ')
+result = create_dict(value)
+print(result)
